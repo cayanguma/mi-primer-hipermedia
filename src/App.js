517a657d4079ffb672a8fd1078a90311b4548796
cayanguma <160347113+cayanguma@users.noreply.github.com>
2024-02-24 19:27:11 +0000

    cambio de texto

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          probando el texto
+          probando el texto en app.js
         </p>
         <a
           className="App-link"
