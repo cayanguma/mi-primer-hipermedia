4de9c4a86cd509110943bb2e23b0d31021f73c08
cayanguma <160347113+cayanguma@users.noreply.github.com>
2024-03-14 04:24:29 +0000

    cambio 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
 <h2>Imagen</h2>
     <img src='https://i.stack.imgur.com/ScRR6m.jpg'></img>
     <h2> esta es la representación grafica de mi haciendo este proyecto </h2>
-    
+<h2> Audio </h2>
+<audio controls=""> <source src="https://samplelib.com/lib/preview/mp3/sample-3s.mp3" type="audio/mpeg">Tu navegador no soporta la etiqueta de audio.</audio>
+<h2> video </h2>
     <iframe width="560" height="315" src="https://www.youtube.com/embed/2yJgwwDcgV8" frameborder="0" allowfullscreen></iframe>
   
 
